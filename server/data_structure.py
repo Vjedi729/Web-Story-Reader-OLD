b9dde8106526ab2456d679ac27448f189adf7680
Goggles <vjedi729@gmail.com>
2019-04-07 08:18:55 -0500

    added conversion from scraped objects to story objects

--- server/data_structure.py
+++ server/data_structure.py
@@ -16,15 +16,59 @@ class WSR_Story:
     Story Parts:
         Title   - String
         Author  - User Reference
-        
+
     '''
-    
+
     def from_FFnet(self, story):
+        self.title          = story.title
+        self.author         = story.author
+        self.rating         = story.maturity_rating
+        self.language       = story.language
+        self.status         = story.status
+        # Tags
+        'TODO: Change tags to match database structure'
+        self.fandoms        = story.fandoms
+        self.genres         = story.genres
+        self.characters     = story.characters
+        self.relationships  = story.romances
+        self.other_tags     = []
+        # Lengths
+        self.word_count     = story.word_count
+        self.chapter_count  = story.chapter_count
+        # Upload Info
+        self.publish_date   = story.publish_date
+        self.update_date    = story.update_date
+        # Interactions
+        self.like_count     = story.fav_count
+        self.follow_count   = story.follow_count
+        self.comment_count  = story.review_count
         pass
-    
+
     def from_Ao3(self, story):
-        pass
-    
+        self.title          = story.title
+        self.author         = story.author
+        self.rating         = story.rating
+        self.language       = story.language
+        self.status         = story.status
+        # Tags
+        'TODO: Change tags to match database structure'
+        self.fandoms        = story.fandoms
+        self.genres         = []
+        self.characters     = story.characters
+        self.relationships  = story.relationships
+        self.other_tags     = story.other_tags
+        # Lengths
+        self.word_count     = story.word_count
+        self.chapter_count  = story.chapter_count
+        # Upload Info
+        self.publish_date   = story.publish_date
+        self.update_date    = story.update_date
+        # Interactions
+        self.like_count     = story.kudo_count
+        self.follow_count   = story.bookmark_count
+        self.comment_count  = story.comment_count
+
+
     def __init__(self, story_type, story):
         if story_type == StoryType.FFnet:
             self.from_FFnet(story)
@@ -32,44 +76,34 @@ class WSR_Story:
             self.from_Ao3(story)
         else:
             raise ValueError("Story Type Not Recognized:" + str(story_type))
-        
+
     def toDict(self):
         d = {}
         # Basic Information
         d['title']          = self.title
         d['author']         = self.author
-        
+        d['rating']         = self.rating
+        d['language']       = self.language
         # Tags
+        d['genres']         = self.genres
+        d['fandoms']        = self.fandoms
         d['characters']     = self.characters
         d['relationships']  = self.relationships
         d['other tags']     = self.other_tags
-        
+
         # Lengths
         d['word_count']     = self.word_count
         d['chapter_count']  = self.chapter_count
-        
+
         # Upload Information
         d['publish date']   = self.publish_date
         d['update data']    = self.update_date
-        
+
         # Interactions
         d['likes']          = self.like_count
         d['follows']        = self.follow_count
         d['comments']       = self.comment_count
         return d
-    
-    def toJson(self):
-        return json.dumps(self.toDict)
-        
-# Removed temporarily
-'''
-class Tag:
-    uid = 0
-    def __init__(self, name):
-        # Give a unique id (within tags)
-        self.id = Tag.uid
-        Tag.uid += 1
 
-        # Insert data:
-        self.name = name
-'''
+    def toJson(self):
+        return json.dumps(self.toDict())
